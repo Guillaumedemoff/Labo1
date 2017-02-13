95e8a75cfd148e132d1a81176d383920c2b01f7b
guillaumedemoff <guillaumedemoff@gmail.com>
2017-02-13 16:03:40 +0100

    Update fact and root

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 2, 2016
-#
+
+import math
+
+
 def fact(n):
     """Computes the factorial of a natural number.
     
@@ -9,7 +12,9 @@ def fact(n):
     Post: Returns the factorial of 'n'.
     Throws: ValueError if n < 0
     """
-
+    answer = math.factorial(n)
+    return answer 
+    
 def roots(a, b, c):
     """Computes the roots of the ax^2 + bx + x = 0 polynomial.
     
@@ -17,7 +22,23 @@ def roots(a, b, c):
     Post: Returns a tuple with zero, one or two elements corresponding
           to the roots of the ax^2 + bx + c polynomial.
     """
-    pass
+    d = b**2-4*a*c
+
+    if d < 0:
+        return -1
+    
+    x1 = (-b+math.sqrt(d))/(2*a)
+    x2 = (-b-math.sqrt(d))/(2*a)
+    print(x1,x2)
+    if x1 == x2:
+        answer = (x1)
+        print(1)
+    else:
+        answer = (x1,x2)
+        print(2)
+
+    return answer
+    
 
 def integrate(function, lower, upper):
     """Approximates the integral of a fonction between two bounds
@@ -28,6 +49,8 @@ def integrate(function, lower, upper):
     Post: Returns an approximation of the integral from 'lower' to 'upper'
           of the specified 'function'.
     """
+
+    # ca, faut demander a Lorge. Merci de votre compréhension 
     pass
 
 if __name__ == '__main__':
